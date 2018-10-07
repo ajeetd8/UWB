e6b0c914c379de939e3af8b83986ee0ce1e59e7f
Haram Kwon <kharam1436@gmail.com>
2018-10-06 20:52:02 -0700

    script fixed again

--- CSS432/Assignment_1/client_run.sh
+++ CSS432/Assignment_1/client_run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "Running the 9 different cases ..."
+
 echo "case 1: 20000 15*100 type:1" > result.txt
-./Client.out 5511 20000 15 100 uw1-320-01.uwb.edu 1  > result.txt
+./Client.out 5511 20000 15 100 uw1-320-01.uwb.edu 1  >> result.txt
 
 echo "case 2 20000 15*100 type:2" >> result.txt
 ./Client.out 5511 20000 15 100 uw1-320-01.uwb.edu 1  >> result.txt
