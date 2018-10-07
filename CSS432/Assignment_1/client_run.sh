61e20b12e9065a982bced13b8bba957678607a6c
Haram Kwon <kharam1436@gmail.com>
2018-10-07 03:48:38 -0700

    client run updated

--- CSS432/Assignment_1/client_run.sh
+++ CSS432/Assignment_1/client_run.sh
@@ -1,29 +1,16 @@
-#!/bin/bash
+#! /bin/bash
 echo "Running the 9 different cases ..."
 
-echo "case 1: 20000 15*100 type:1" > result.txt
-./Client.out 5511 20000 15 100 uw1-320-01.uwb.edu 1  >> result.txt
+PORT=9190
+REPETITION=20000
+SERVER=$1
 
-echo "case 2 20000 15*100 type:2" >> result.txt
-./Client.out 5511 20000 15 100 uw1-320-01.uwb.edu 2  >> result.txt
-
-echo "case 3 20000 15*100 type:3" >> result.txt
-./Client.out 5511 20000 15 100 uw1-320-01.uwb.edu 3  >> result.txt
-
-echo "case 4 20000 30*50 type:1" >> result.txt
-./Client.out 5511 20000 30 50 uw1-320-01.uwb.edu 1   >> result.txt
-
-echo "case 5 20000 30*50 type 2" >> result.txt
-./Client.out 5511 20000 30 50 uw1-320-01.uwb.edu 2   >> result.txt
-
-echo "case 6 20000 30*50 type 3" >> result.txt
-./Client.out 5511 20000 30 50 uw1-320-01.uwb.edu 3   >> result.txt
-
-echo "case 7 20000 60*25 type 1" >> result.txt
-./Client.out 5511 20000 60 25 uw1-320-01.uwb.edu 1   >> result.txt
-
-echo "case 8 20000 60*25 type 2" >> result.txt
-./Client.out 5511 20000 60 25 uw1-320-01.uwb.edu 2   >> result.txt
-
-echo "case 9 20000 60*25 type 3" >> result.txt
-./Client.out 5511 20000 60 25 uw1-320-01.uwb.edu 3   >> result.txt
+for NBUF in 15 30 60
+do
+    BUFSIZE=$((1500 / $NBUF))
+    
+    for TYPE in 1 2 3
+    do
+        ./client $PORT $REPETITION $NBUF $BUFSIZE $SERVER $TYPE
+    done
+done
