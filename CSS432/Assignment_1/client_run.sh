b52ae7c71414c859e07486d22fd28ca6c8fc02d5
Haram Kwon <kharam1436@gmail.com>
2018-10-06 20:47:26 -0700

    client run script fixed

--- CSS432/Assignment_1/client_run.sh
+++ CSS432/Assignment_1/client_run.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
+echo "Running the 9 different cases ..."
+"case 1: 20000 15*100 type:1" > result.txt
+./Client.out 5511 20000 15 100 uw1-320-01.uwb.edu 1  > result.txt
 
-./Client.out 5511 20000 15 100 127.0.0.1 1  > out.txt
-./Client.out 5511 20000 15 100 127.0.0.1 1  >> out.txt
-./Client.out 5511 20000 15 100 127.0.0.1 1  > 1
-./Client.out 5511 20000 30 50 127.0.0.1 1   > 1
-./Client.out 5511 20000 30 50 127.0.0.1 1   > 1
-./Client.out 5511 20000 30 50 127.0.0.1 1   > 1
-./Client.out 5511 20000 60 25 127.0.0.1 1   > 1
-./Client.out 5511 20000 60 25 127.0.0.1 1   > 1
-./Client.out 5511 20000 60 25 127.0.0.1 1   > 1
+"case 2 20000 15*100 type:2"
+./Client.out 5511 20000 15 100 uw1-320-01.uwb.edu 1  >> result.txt
+
+"case 3 20000 15*100 type:3"
+./Client.out 5511 20000 15 100 uw1-320-01.uwb.edu 1  >> result.txt
+
+"case 4 20000 30*50 type:1"
+./Client.out 5511 20000 30 50 uw1-320-01.uwb.edu 1   >> result.txt
+
+"case 5 20000 30*50 type 2"
+./Client.out 5511 20000 30 50 uw1-320-01.uwb.edu 1   >> result.txt
+
+"case 6 20000 30*50 type 3"
+./Client.out 5511 20000 30 50 uw1-320-01.uwb.edu 1   >> result.txt
+
+"case 7 20000 60*25 type 1"
+./Client.out 5511 20000 60 25 uw1-320-01.uwb.edu 1   >> result.txt
+
+"case 8 20000 60*25 type 2"
+./Client.out 5511 20000 60 25 uw1-320-01.uwb.edu 1   >> result.txt
+
+"case 9 20000 60*25 type 3"
+./Client.out 5511 20000 60 25 uw1-320-01.uwb.edu 1   >> result.txt
