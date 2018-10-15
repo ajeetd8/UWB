fe0e452a98fbf2170bb867cc23b3b8e07f20431a
Haram Kwon <kharam1436@gmail.com>
2018-10-15 08:59:10 -0700

    commented added, with pipe implement improvement

--- Assignment_1/processes.cpp
+++ Assignment_1/processes.cpp
@@ -1,54 +1,62 @@
-// 
+// Copyright 2018 Haram Kwon
+
 #include <cstdio>
 #include <iostream>
 #include <unistd.h>
 #include <sys/wait.h>
 
+// File decryptor for reading and writing
 enum {FD_READ, FD_WRITE};
 
+// Main function to handle
 int main(int argc, char* argv[])
 {
+    // craete wc, grep, and ps pid.
+    // create two pipes.
+    // Make status variable to get return code from child process.
     pid_t wc_pid, grep_pid, ps_pid;
-    int fd_read[2], fd_write[2];
+    int pipe1[2], pipe2[2];
     int status;
 
     // Requesting pipe to Operating System.
-    pipe(fd_read), pipe(fd_write);
+    pipe(pipe1), pipe(pipe2);
 
     // make child to handle 'wc -l' command.
     wc_pid = fork();
     if(wc_pid == 0) {
-        close(fd_write[FD_WRITE]);
-        close(fd_read[FD_WRITE]);
-        dup2(fd_write[FD_READ], 0);
+        // read from pipe2 as if stdin.
+        close(pipe2[FD_WRITE]);
+        close(pipe1[FD_WRITE]);
+        close(pipe1[FD_READ]);
+        dup2(pipe2[FD_READ], 0);
         execlp("/usr/bin/wc", "wc", "-l", 0);
     }
     else {
         // make child to handle 'grep `argv[1]`' command.
         grep_pid = fork();
         if(grep_pid == 0) {
-            // wait(&status);
-            // dup2(fd_read[FD_READ], 0);
-            // close(fd_read[FD_WRITE]);=
-                close(fd_read[FD_WRITE]);
-                dup2(fd_read[FD_READ], 0);
-                dup2(fd_write[FD_WRITE], 1);
-                execlp("/bin/grep", "grep", argv[1], 0);
+            // It's going to get string from pipe 1 as if stdin.
+            // grep command with arguement.
+            // write string to pipe 2 as if stdout.
+            close(pipe1[FD_WRITE]);
+            close(pipe1[FD_WRITE]);
+            dup2(pipe1[FD_READ], 0);
+            dup2(pipe2[FD_WRITE], 1);
+            execlp("/bin/grep", "grep", argv[1], 0);
         }
         else {
             // make child to handle 'ps -A' command.
             ps_pid = fork();
             if(ps_pid == 0) {
-                // close(fd_read[FD_WRITE]);
-                close(fd_read[FD_READ]);
-                close(fd_write[FD_WRITE]);
-                close(fd_read[FD_READ]);
-                dup2(fd_read[FD_WRITE], 1);     // stdout to pipe_read
+                // use pipe 1 as if stdin.
+                close(pipe1[FD_READ]);
+                close(pipe2[FD_WRITE]);
+                close(pipe1[FD_READ]);
+                dup2(pipe1[FD_WRITE], 1);     // stdout to pipe_read
                 execlp("/bin/ps", "ps", "-A", 0);
             }
             else {
                 wait(&status);
-                
             }
         }
         
