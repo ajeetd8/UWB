accb19e4beb1605f43e573f9a36028e4b8723c45
Max Wright <maxwright378@gmail.com>
2018-11-29 18:53:04 -0800

    A Message

--- FileSystem.java
+++ FileSystem.java
@@ -1,3 +1,17 @@
 public class FileSystem {
+    private Superblock superblock = new Superblock(10);
+    private List<Inode> inodes = new ArrayList<>();
+
+    public FileSystem() {
+
+    }
+
+    public int format(int files) {
+        if(files <= 0) {
+            return -1;
+        }
+        superblock.totalInodes = files;
+        return 0;
+    }
 
 }
