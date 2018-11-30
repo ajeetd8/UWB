7e777d65129b111b0589c9a8a12b11453447401c
Haram Kwon <kharam1436@gmail.com>
2018-11-29 19:08:54 -0800

    File system listed

--- FileSystem.java
+++ FileSystem.java
@@ -1,3 +1,37 @@
 public class FileSystem {
 
+    public FileSystem ( ) {
+
+    }
+    
+    public int format( int files );  {
+    }
+
+    public int open( String fileName, String mode ) {
+
+    }
+
+    public int read( int fd, byte buffer[] ) {
+
+    }
+
+    public int write( int fd, byte buffer[] ) {
+
+    }
+
+    public int seek( int fd, int offset, int whence ) {
+
+    }
+
+    public int close( int fd ) {
+
+    }
+
+    public int delete( String fileName ) {
+
+    }
+
+    public int fsize( int fd ) {
+
+    }
 }
