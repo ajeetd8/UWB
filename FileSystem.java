cc3b64e454f878156c308e93da1712cfc39a518a
Haram Kwon <kharam1436@gmail.com>
2018-12-04 16:25:48 -0800

    Error detecting error case added

--- FileSystem.java
+++ FileSystem.java
@@ -26,30 +26,37 @@ public class FileSystem {
     }
 
     public int open( String fileName, String mode ) {
-
+        // Error detecting
+        return -1;
     }
 
     public int read( int fd, byte buffer[] ) {
-
+        // Error detecting
+        return -1;
     }
 
     public int write( int fd, byte buffer[] ) {
-
+        // Error detecting
+        return -1;
     }
 
     public int seek( int fd, int offset, int whence ) {
-
+        // Error detecting
+        return -1;
     }
 
     public int close( int fd ) {
-
+        // Error detecting
+        return -1;
     }
 
     public int delete( String fileName ) {
-
+        // Error detecting
+        return -1;
     }
 
     public int fsize( int fd ) {
-
+        // Error detecting
+        return -1;
     }
 }
