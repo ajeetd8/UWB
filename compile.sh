4f8f232d3fa41376d10235ac4599fceb51ce8e14
Haram Kwon <kharam1436@gmail.com>
2018-12-06 12:37:35 -0800

    compile code added

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# javac -Xlint:unchecked FileSystem.java ;
-# javac -Xlint:unchecked Kernel.java ;
-# javac -Xlint:unchecked SuperBlock.java ;
-# javac -Xlint:unchecked Inode.java ;
-# javac -Xlint:unchecked Directory.java ;
-# javac -Xlint:unchecked FileTable.java ;
-# javac -Xlint:unchecked TCB.java ;
-# javac -Xlint:unchecked SysLib.java
+javac -Xlint:unchecked FileSystem.java ;
+javac -Xlint:unchecked Kernel.java ;
+javac -Xlint:unchecked SuperBlock.java ;
+javac -Xlint:unchecked Inode.java ;
+javac -Xlint:unchecked Directory.java ;
+javac -Xlint:unchecked FileTable.java ;
+javac -Xlint:unchecked TCB.java ;
+javac -Xlint:unchecked SysLib.java
