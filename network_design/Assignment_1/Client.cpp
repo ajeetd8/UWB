622d208d4748281ee34b53dfc82db100ba1d0bdf
Haram Kwon <kharam1436@gmail.com>
2018-12-19 01:38:17 -0800

    the line alignment has been changed

--- network_design/Assignment_1/Client.cpp
+++ network_design/Assignment_1/Client.cpp
@@ -34,134 +34,145 @@
 int main(int argc, char *argv[])
 {
     // Checking the number of the argument
-    if (argc != 7) {
+    if (argc != 7)
+    {
         printf("Usage : %s <server_port> <repetition> <nbufs> ", argv[0]);
         printf("<bufsize> <serverIp> <type> \n");
         return -1;
     }
-    
+
     // Checking valid ports
-    if (atoi(argv[1]) < 1023 || atoi(argv[1]) > 65536) {
+    if (atoi(argv[1]) < 1023 || atoi(argv[1]) > 65536)
+    {
         std::cerr << "valid server_port range is 1024 to 65536." << std::endl;
         return -1;
     }
-    
+
     // Checkig valid repetition(s)
-    if(atoi(argv[2]) < 0)
+    if (atoi(argv[2]) < 0)
     {
         std::cerr << "repetitions must be a positive integer." << std::endl;
         return -1;
     }
-    
+
     // checking buf size
     if (atoi(argv[3]) * atoi(argv[4]) != BUFFSIZE)
     {
         std::cerr << "nbufs * bufsize must equal 1500." << std::endl;
         return -1;
     }
-    
+
     // checking server_port number
     if (atoi(argv[6]) > 3 || atoi(argv[6]) < 1)
     {
         std::cerr << "valid types are 1, 2, or 3." << std::endl;
         return -1;
     }
-    
+
     // Saving argument to variable
     const int server_port = atoi(argv[1]);
     const int repetition = atoi(argv[2]);
     const int nbufs = atoi(argv[3]);
     const int bufsize = atoi(argv[4]);
-    char * server = argv[5];
+    char *server = argv[5];
     const int type = atoi(argv[6]);
-    
+
     //Get Hostent structure to communicate to server.
     struct hostent *host = gethostbyname(server);
-    if (host == NULL) {
+    if (host == NULL)
+    {
         std::cerr << "Error:  Could not find hostname." << std::endl;
         return -1;
     }
-    
+
     // Build the sending socket address of client
     // Setting the server setting
     sockaddr_in sendSocketAddress;
     bzero((char *)&sendSocketAddress, sizeof(sendSocketAddress));
-    sendSocketAddress.sin_family = AF_INET;     // Address Family:  Internet
-    sendSocketAddress.sin_addr.s_addr = inet_addr(inet_ntoa(*(struct in_addr*) (*host->h_addr_list)));
+    sendSocketAddress.sin_family = AF_INET; // Address Family:  Internet
+    sendSocketAddress.sin_addr.s_addr = inet_addr(inet_ntoa(*(struct in_addr *)(*host->h_addr_list)));
     sendSocketAddress.sin_port = htons(server_port);
-    
+
     // Setting the server decreptor from client
     int clientSD = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSD < 0) {
+    if (clientSD < 0)
+    {
         std::cerr << "socket() error" << std::endl;
         close(clientSD);
         return -1;
     }
-    
+
     //Connect this TCP socket with the server
     int connetStat = connect(clientSD, (sockaddr *)&sendSocketAddress, sizeof(sendSocketAddress));
-    if (connetStat < 0) {
+    if (connetStat < 0)
+    {
         //negative connetStat indicates failure
         std::cerr << "connect() error!" << std::endl;
         close(clientSD);
         return -1;
     }
 
-    if(connect(clientSD, (sockaddr *)&sendSocketAddress, sizeof(sendSocketAddress)) == -1)
+    if (connect(clientSD, (sockaddr *)&sendSocketAddress, sizeof(sendSocketAddress)) == -1)
     {
         // Error handling
     }
 
-    
-    //Allocate the databuffer. 
+    //Allocate the databuffer.
     char databuf[nbufs][bufsize];
-    
+
     //Use timeval structs for gettimeofday to track total usage time.
     struct timeval start, lap, stop;
 
     //Record start time.
     gettimeofday(&start, NULL);
-    
+
     // With different case, send different form of data
-    for (int i = 0; i < repetition; i++) {
+    for (int i = 0; i < repetition; i++)
+    {
         // Based on the type, do some task.
-        if (type == 1) {
+        if (type == 1)
+        {
             for (int j = 0; j < nbufs; j++)
-                write(clientSD, databuf[j], bufsize);  // sd: socket descriptor
-        } else if (type == 2) {
+                write(clientSD, databuf[j], bufsize); // sd: socket descriptor
+        }
+        else if (type == 2)
+        {
             struct iovec vector[nbufs];
-            for (int j = 0; j < nbufs; j++) {
+            for (int j = 0; j < nbufs; j++)
+            {
                 vector[j].iov_base = databuf[j];
                 vector[j].iov_len = bufsize;
             }
-            writev(clientSD, vector, nbufs);  // sd: socket descriptor
-        } else {
-            write(clientSD, databuf, nbufs * bufsize);  // sd: socket descriptor
+            writev(clientSD, vector, nbufs); // sd: socket descriptor
+        }
+        else
+        {
+            write(clientSD, databuf, nbufs * bufsize); // sd: socket descriptor
         }
     }
-    
+
     //Record write end time.
     gettimeofday(&lap, NULL);
-    
+
     //Receive from the server an integer acknowledgment that shows how many
     //times the server called read().
-    int count;      //read from the server.
+    int count; //read from the server.
     read(clientSD, &count, sizeof(count));
-    
+
     //Record total round-trip end time.
     gettimeofday(&stop, NULL);
-    
+
     // Commulating time
-    long lapTime = (lap.tv_sec-start.tv_sec)*1000000
-        +(lap.tv_usec-start.tv_usec);
-    long endTime = (stop.tv_sec-start.tv_sec)*1000000
-        +(stop.tv_usec-start.tv_usec);
+    long lapTime = (lap.tv_sec - start.tv_sec) * 1000000 + (lap.tv_usec - start.tv_usec);
+    long endTime = (stop.tv_sec - start.tv_sec) * 1000000 + (stop.tv_usec - start.tv_usec);
 
     // Printing out statistics.
-    std::cout << "Test 1" << ": data-sending time = "
-    << lapTime << " usec, " << "round-trip time = " << endTime
-    << " usec, #reads = " << count << std::endl;
-    
+    std::cout << "Test 1"
+              << ": data-sending time = "
+              << lapTime << " usec, "
+              << "round-trip time = " << endTime
+              << " usec, #reads = " << count << std::endl;
+
     //End this session.
     close(clientSD);
     return 0;
--- network_design/Assignment_1/Server.cpp
+++ network_design/Assignment_1/Server.cpp
@@ -8,7 +8,7 @@
  * 
  * The server will read data from the packet from client, and cound how many
  * read invoked, and return to the client
- */ 
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,7 +24,7 @@
 // Defining the bufsize
 #define BUFSIZE 1500
 
-void * handler(void *sock);
+void *handler(void *sock);
 
 // Locking global variable
 pthread_mutex_t mutex;
@@ -32,9 +32,11 @@ pthread_mutex_t mutex;
 // Global variable to share
 int repetition;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     // Checking the number of argument
-    if (argc != 3) {
+    if (argc != 3)
+    {
         printf("Usage : %s <port> <repetition>\n", argv[0]);
         return -1;
     }
@@ -46,11 +48,12 @@ int main(int argc, char *argv[]) {
     ::repetition = atoi(argv[2]);
 
     serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-    if (serv_sock == -1) {
-        std::cerr<<"socket() error"<<std::endl;
+    if (serv_sock == -1)
+    {
+        std::cerr << "socket() error" << std::endl;
         return -1;
     }
-    bzero((char*)&serv_adr, sizeof(serv_adr));
+    bzero((char *)&serv_adr, sizeof(serv_adr));
     serv_adr.sin_family = AF_INET;
     serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_adr.sin_port = htons(atoi(argv[1]));
@@ -59,37 +62,41 @@ int main(int argc, char *argv[]) {
     const int on = 1;
     setsockopt(serv_sock, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(int));
 
-    if (bind(serv_sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr)) == -1) {
-        std::cerr<<"bind() error"<<std::endl;
+    if (bind(serv_sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr)) == -1)
+    {
+        std::cerr << "bind() error" << std::endl;
         return -1;
     }
-    if (listen(serv_sock, 5) == -1) {
-        std::cerr<<"listen() error"<<std::endl;
+    if (listen(serv_sock, 5) == -1)
+    {
+        std::cerr << "listen() error" << std::endl;
     }
 
     // Server never turned off
-    while (true) {
+    while (true)
+    {
         clnt_sock = accept(serv_sock,
-        (struct sockaddr *)&clnt_adr, &clnt_adr_sz);
+                           (struct sockaddr *)&clnt_adr, &clnt_adr_sz);
 
         // Check for the accept error
         if (clnt_sock == -1)
             continue;
-        else  // no accept error, make new thread
+        else // no accept error, make new thread
             pthread_create(&t_id, NULL, handler,
-            reinterpret_cast<void *>(&clnt_sock));
-            pthread_detach(t_id);
+                           reinterpret_cast<void *>(&clnt_sock));
+        pthread_detach(t_id);
     }
 
     close(serv_sock);
     return 0;
 }
 
-void * handler(void *sock) {
+void *handler(void *sock)
+{
     // Begin to measur the time
     timeval startTime, endTime;
     gettimeofday(&startTime, NULL);
-    
+
     // Allocate databuffer
     char buf[BUFSIZ];
 
@@ -97,18 +104,20 @@ void * handler(void *sock) {
     int sd = *((int *)sock);
     __suseconds_t duration;
 
-    for(int i=0; i<(::repetition); i++) {
+    for (int i = 0; i < (::repetition); i++)
+    {
         // Read everything.
         for (int nRead = 0;
-            ((nRead += read(sd, buf, BUFSIZE - nRead))) < BUFSIZE;
-            ++count) {}
+             ((nRead += read(sd, buf, BUFSIZE - nRead))) < BUFSIZE;
+             ++count)
+        {
+        }
     }
 
     // Stop measuring time
     gettimeofday(&endTime, NULL);
-    duration = (endTime.tv_sec-startTime.tv_sec)*1000000
-        +(endTime.tv_usec-startTime.tv_usec);
-    
+    duration = (endTime.tv_sec - startTime.tv_sec) * 1000000 + (endTime.tv_usec - startTime.tv_usec);
+
     std::cout << "data-receiving time = " << duration << " usec" << std::endl;
 
     // Convert the number to Big-Endian
